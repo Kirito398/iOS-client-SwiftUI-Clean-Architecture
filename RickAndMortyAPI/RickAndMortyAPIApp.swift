6c6a9e99c72554e2fd257b895e40335e4a574e46
SIR <henkok398@mail.com>
2024-09-30 14:53:15 +0900

    Initial Commit

---
+++ RickAndMortyAPI/RickAndMortyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAPIApp.swift
+//  RickAndMortyAPI
+//
+//  Created by Ilgiz Sultanbekov on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
